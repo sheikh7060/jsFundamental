419b34b9dd9f13f3493edc3498d88eee0a7e79a7
sheikh7060 <mdsheikhsagor@gmail.com>
2025-03-07 19:40:40 +0600

    update Rock Paper Scissor Game

---
+++ function/script.js
@@ -0,0 +1,11 @@
+
+function taxCalculator(totalAmount,taxRate= 0.1){
+    console.log(totalAmount*taxRate);
+}
+
+taxCalculator(120,0.1);
+taxCalculator(80,0.2);
+taxCalculator(500,0.05);
+taxCalculator(120)
+taxCalculator(130)
+taxCalculator(11);
---
+++ object/script.js
@@ -0,0 +1,11 @@
+const prayerTime = {
+
+    fazr: '5:30 Am',
+    zahr: '1:30 Pm',
+    asr: '4:45 Pm',
+    magrib: '6:22 Pm',
+    esa : '8:15 Pm' 
+}
+prayerTime.tarabi = 'After esa to until Fazr like 9:pm to 5 am';
+delete prayerTime.tarabi;
+console.log(prayerTime);
